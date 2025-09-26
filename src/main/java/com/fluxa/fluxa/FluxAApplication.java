b7a5905d8e4bf0b1cf9cc7ae1975ae514b031a0b
nskalu <Nollykalz@1>
2025-09-25 22:33:46 -0600

    initial project setup

---
+++ src/main/java/com/fluxa/fluxa/FluxAApplication.java
@@ -0,0 +1,13 @@
+package com.fluxa.fluxa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluxa/fluxa/FluxAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxa.fluxa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
